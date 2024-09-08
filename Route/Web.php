d46771d2434a617e66fee16eedf8284c9c8aceec
zahra boojar <zahraboojar866@gmail.com>
2024-09-08 11:41:45 +0330

    add web route

--- Route/Web.php
+++ Route/Web.php
@@ -4,9 +4,37 @@ use App\Controllers\UserController;
 use App\Controllers\PostController;
 use App\Controllers\IndexController;
 
-
-Flight::group("/panel", function(){
+Flight::group("/panel", function () {
 
     Flight::route("GET /", [new IndexController, "panel_index"]);
+    Flight::route("GET /login", [new UserController, "panel_login"]);
+    Flight::route("GET /logout", [new UserController, "panel_logout"]);
 
+    Flight::group("/manage", function () {
+        Flight::route("GET /posts", [new PostController, "panel_manage_posts"]);
+        Flight::route("GET /users", [new UserController, "panel_manage_users"]);
+        Flight::route("GET /setting", [new IndexController, "panel_manage_setting"]);
+        Flight::route("GET /account", [new PostController, "panel_manage_account"]);
+    });
+
+    Flight::group("/posts", function () {
+        Flight::route("GET /", [new PostController, "panel_posts"]);
+        Flight::route("GET /@id", [new UserController, "panel_show_post"]);
+    });
+
+    Flight::group("/users", function () {
+        Flight::route("GET /", [new PostController, "panel_users"]);
+        Flight::route("GET /@id", [new UserController, "panel_show_user"]);
+    });
+
+    Flight::group("/result", function () {
+        Flight::route("POST /login", [new PostController, "panel_result_login"]);
+        Flight::route("POST /create-post", [new UserController, "panel_result_create_post"]);
+        Flight::route("GET /delete-post/@id", [new UserController, "panel_result_delete_post"]);
+        Flight::route("POST /update-post/@id", [new UserController, "panel_result_update_post"]);
+        Flight::route("POST /create-user", [new UserController, "panel_result_create_user"]);
+        Flight::route("GET /delete-user/@id", [new UserController, "panel_result_delete_user"]);
+        Flight::route("POST /update-user/@id", [new UserController, "panel_result_update_user"]);
+    });
 });
+
