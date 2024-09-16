0b19b380974ad87cdde260c2ecfd62e40ed6181a
zahra boojar <zahraboojar866@gmail.com>
2024-09-16 12:57:00 +0330

    update sql file

--- site2.sql
+++ site2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 10, 2024 at 12:00 PM
+-- Generation Time: Sep 16, 2024 at 11:26 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -32,6 +32,7 @@ CREATE TABLE `comments` (
   `title` varchar(5000) NOT NULL,
   `comment` text NOT NULL,
   `user_id` bigint(20) UNSIGNED NOT NULL,
+  `post_id` bigint(20) NOT NULL,
   `date` date NOT NULL,
   `state` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_persian_ci;
@@ -40,8 +41,9 @@ CREATE TABLE `comments` (
 -- Dumping data for table `comments`
 --
 
-INSERT INTO `comments` (`id`, `title`, `comment`, `user_id`, `date`, `state`) VALUES
-(1, 'my comment', 'thanks', 2, '2024-09-20', 0);
+INSERT INTO `comments` (`id`, `title`, `comment`, `user_id`, `post_id`, `date`, `state`) VALUES
+(1, 'my comment', 'thanks', 2, 1, '2024-09-20', 0),
+(2, 'realy', 'but it dont work', 3, 2, '2024-09-10', 1);
 
 -- --------------------------------------------------------
 
@@ -66,8 +68,8 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `title`, `content`, `image`, `admin_id`, `date`, `viewcount`, `state`, `slug`) VALUES
-(1, 'hello', '.....', 'color-palette-guide-sample-colors-catalog-.jpg', 1, '2024-09-10', 0, 1, 'dddd'),
-(2, 'bye', '....', 'group-of-people-brainstorming-and-taking-notes-4.jpg', 1, '2024-09-12', 3, 0, 'fff');
+(1, 'hello', '.....', 'color-palette-guide-sample-colors-catalog-.jpg', 1, '2024-09-10', 8, 1, 'dddd'),
+(2, 'bye', '....', 'group-of-people-brainstorming-and-taking-notes-4.jpg', 2, '2024-09-12', 3, 0, 'fff');
 
 -- --------------------------------------------------------
 
@@ -92,7 +94,8 @@ CREATE TABLE `settings` (
 INSERT INTO `settings` (`id`, `key_setting`, `value_setting`, `link`, `title`, `text`, `type`) VALUES
 (1, 'logo', 'logo.svg', '/', 'logo img', '', 'setting'),
 (2, 'footer', 'Asrez', 'https://asrez.ir', 'footer', '', 'setting'),
-(3, 'title', 'Tabler', '/', 'title', '', 'setting');
+(3, 'title', 'Tabler', '/', 'title', '', 'setting'),
+(4, 'adver1', 'google', 'www.google.com', 'google', 'its a new word ', 'adver');
 
 -- --------------------------------------------------------
 
@@ -117,7 +120,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `username`, `email`, `password`, `image`, `date`, `state`) VALUES
 (1, 'zahra', 'zahra', 'zahra@gmail.com', '202cb962ac59075b964b07152d234b70', 'default.png', '2024-09-04', 1),
-(2, 'ali', 'ali', 'ali@gmail.com', '202cb962ac59075b964b07152d234b70', 'default.png', '2024-09-18', 0);
+(2, 'ali', 'ali', 'ali@gmail.com', '202cb962ac59075b964b07152d234b70', 'default.png', '2024-09-18', 0),
+(3, 'vahid', 'vahid', 'vahid@gmail.com', '202cb962ac59075b964b07152d234b70', 'default.png', '2024-09-04', 0);
 
 -- --------------------------------------------------------
 
@@ -174,7 +178,7 @@ ALTER TABLE `views`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `posts`
@@ -186,13 +190,13 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT for table `settings`
 --
 ALTER TABLE `settings`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `views`
